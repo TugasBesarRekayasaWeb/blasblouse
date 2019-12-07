e915f2270314aa389377aa325d60be06b60baa4b
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-12-07 12:01:34 +0700

    membuat controler

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+     public function home()
+    {
+        return view('index');
+    }
+
+
+}
